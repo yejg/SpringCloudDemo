51345247918a64ae419c2647e8aa2894ba947371
yejg <yejg@cairenhui.com>
2018-07-23 14:08:20 +0800

    test check plugin

--- zuul/src/main/java/com/ye/app/ZuulApplication.java
+++ zuul/src/main/java/com/ye/app/ZuulApplication.java
@@ -1,21 +1,24 @@
 package com.ye.app;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+/**
+ * 
+ * @author yejg
+ *
+ */
 @EnableZuulProxy
 @SpringCloudApplication
 public class ZuulApplication {
 
 	public static void main(String[] args) {
-		Integer tmp;
-		Pattern pattern = Pattern.compile("_crh_user=(.+?);");
-		Matcher matcher = pattern.matcher("_crh_user=fdsaafsagagafgfgfdgfagad");
-		System.out.println(matcher.find());
+//		Integer tmp;
+//		Pattern pattern = Pattern.compile("_crh_user=(.+?);");
+//		Matcher matcher = pattern.matcher("_crh_user=fdsaafsagagafgfgfdgfagad");
+//		System.out.println(matcher.find());
+		System.out.println("testtest");
 		SpringApplication.run(ZuulApplication.class, args);
 	}
 }
