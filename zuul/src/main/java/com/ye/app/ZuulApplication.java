f6bb00184082931a099754550618fbe4391ced05
yejg <yejg@cairenhui.com>
2018-07-23 14:00:07 +0800

    # WARNING: head commit changed in the meantime
    test

--- zuul/src/main/java/com/ye/app/ZuulApplication.java
+++ zuul/src/main/java/com/ye/app/ZuulApplication.java
@@ -1,5 +1,8 @@
 package com.ye.app;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -9,8 +12,10 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class ZuulApplication {
 
 	public static void main(String[] args) {
-		int a;
-		int b;
+		Integer tmp;
+		Pattern pattern = Pattern.compile("_crh_user=(.+?);");
+		Matcher matcher = pattern.matcher("_crh_user=fdsaafsagagafgfgfdgfagad");
+		System.out.println(matcher.find());
 		SpringApplication.run(ZuulApplication.class, args);
 	}
 }
