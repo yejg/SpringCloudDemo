631b36fa3b355be157b7c2009e7c8e261656b708
yejg <yejg@cairenhui.com>
2018-07-23 14:47:46 +0800

    test commit check

--- zuul/src/main/java/com/ye/app/ZuulApplication.java
+++ zuul/src/main/java/com/ye/app/ZuulApplication.java
@@ -1,5 +1,8 @@
 package com.ye.app;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -15,9 +18,9 @@ public class ZuulApplication {
 
 	public static void main(String[] args) {
 //		Integer tmp;
-//		Pattern pattern = Pattern.compile("_crh_user=(.+?);");
-//		Matcher matcher = pattern.matcher("_crh_user=fdsaafsagagafgfgfdgfagad");
-//		System.out.println(matcher.find());
+		Pattern pattern = Pattern.compile("_crh_user=(.+?);");
+		Matcher matcher = pattern.matcher("_crh_user=fdsaafsagagafgfgfdgfagad");
+		System.out.println(matcher.find());
 		System.out.println("testtest1111");
 		SpringApplication.run(ZuulApplication.class, args);
 	}
