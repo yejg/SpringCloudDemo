6893145432b8d220799914dc98afdd38191931a3
yejg <yejg@cairenhui.com>
2018-07-24 14:08:55 +0800

    test

--- zuul/src/main/java/com/ye/app/ZuulApplication.java
+++ zuul/src/main/java/com/ye/app/ZuulApplication.java
@@ -7,21 +7,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-/**
- * 
- * @author yejg
- *
- */
+
 @EnableZuulProxy
 @SpringCloudApplication
 public class ZuulApplication {
 
 	public static void main(String[] args) {
 //		Integer tmp;
-		Pattern pattern = Pattern.compile("_crh_user=(.+?);");
-		Matcher matcher = pattern.matcher("_crh_user=fdsaafsagagafgfgfdgfagad");
-		System.out.println(matcher.find());
-		System.out.println("test plugin check");
+//		Pattern pattern = Pattern.compile("_crh_user=(.+?);");
+//		Matcher matcher = pattern.matcher("_crh_user=fdsaafsagagafgfgfdgfagad");
+//		System.out.println(matcher.find());
+		System.out.println("test plugin check11");
 		SpringApplication.run(ZuulApplication.class, args);
 	}
 }
